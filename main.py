6600fb80e21c3822580c3b77b278d6ec3a78b1c6
moshe <moshekimhi91@gmail.com>
2020-08-23 14:26:22 +0300

    q learning plotting reward exported graph data to csv tasks: 1. define loss and print it 2. save q table in csv

--- main.py
+++ main.py
@@ -13,24 +13,28 @@ def create_summeries_dir(name):
 if __name__ == '__main__':
     config = read_main_config()
     gym_wrapper = GymWrapper(config['general']['scenario'])
+
+
     summaries_dir = create_summeries_dir('Frozen_lake_Trajectory_buffer')
-    summaries_collector = SummariesCollector(summaries_dir, 'Frozen_lake_Trajectory_buffer', config)
+    summaries_collector_nt = SummariesCollector(summaries_dir, config)
     q_learner = QLearning(config, gym_wrapper,trajectory = True)
     initial_time = round(time(), 3)
-    q_learner.train(summaries_collector)
-    q_learner.test(summaries_collector,episodes=50,render=True)
-    summaries_collector.read_summaries('test')
-    total_time = round(time(),3)-initial_time
-    print("total train and test time for trajectory replay buffer: {0} seconds".format(total_time))
+    q_learner.train(summaries_collector_nt)
+    trajectory_test_reward= q_learner.test(summaries_collector_nt,episodes=50,render=True)
+    summaries_collector_nt.read_summaries('test')
+    total_time_traj = round(time(),3)-initial_time
+    print("total train and test time for trajectory replay buffer: {0} seconds".format(total_time_traj))
 
     summaries_dir = create_summeries_dir('Frozen_lake_Regular_buffer')
-    summaries_collector = SummariesCollector(summaries_dir, 'Frozen_lake_Regular_buffer', config)
+    summaries_collector = SummariesCollector(summaries_dir, config)
     q_learner = QLearning(config, gym_wrapper, trajectory=False)
     initial_time = round(time(), 3)
     q_learner.train(summaries_collector)
-    q_learner.test(summaries_collector, episodes=50, render=True)
+    no_trajectory_test_reward= q_learner.test(summaries_collector, episodes=50, render=True)
     summaries_collector.read_summaries('test')
     total_time = round(time(), 3) - initial_time
-    print("total train and test time for no trajectory replay buffer: {0} seconds".format(total_time))
 
+    print("tested avg reward with regular buffer: {0}, with trajectory buffer: {1}".format(no_trajectory_test_reward, trajectory_test_reward))
+    print("total train and test time for no trajectory replay buffer: {0} seconds".format(total_time))
+    print("total train and test time for trajectory replay buffer: {0} seconds".format(total_time_traj))
 
--- q_learner.py
+++ q_learner.py
@@ -13,6 +13,8 @@ class QLearning:
         self.q_table = np.zeros((env.observation_space.n, env.action_space.n))
         buf_size = self.config['model']['replay_buffer_size']
         self.replay_buffer = TrajectoryReplayBuffer(buf_size) if trajectory else ReplayBuffer(buf_size)
+        self.tests=0
+        #self.loss=[]
 
     def train(self,summaries_collector):
         env = self.gym_wrapper.get_env()
@@ -38,7 +40,7 @@ class QLearning:
             if (cycle%1000 ==0):
                 summaries_collector.read_summaries('train')
             if (cycle + 1) % self.config['general']['test_frequency'] == 0 or (completion_reward is not None and train_avg_reward > completion_reward):
-                test_avg_reward = self.test(summaries_collector,True)
+                test_avg_reward = self.test(summaries_collector)
                 if completion_reward is not None and test_avg_reward > completion_reward:
                     print('TEST avg reward {} > required reward {}... stopping training'.format(test_avg_reward, completion_reward))
                     break
@@ -57,7 +59,7 @@ class QLearning:
             rewards_per_episode.append(sum(rewards))
             cycle_len += len(actions)
         avg_rewards = np.mean(rewards_per_episode)
-        if (avg_rewards > 0.2 ):
+        if (avg_rewards > 0.5 ):
             print('collected rewards: {}'.format(avg_rewards))
         epsilon *= self.config['model']['decrease_epsilon']
         epsilon = max(epsilon, self.config['model']['min_epsilon'])
@@ -83,10 +85,13 @@ class QLearning:
             q_label = np.array(reward[i]) + gamma*(1. - is_terminal[i]) * max_next_q_value
             curr_q_label = self.q_table[current_state[i], chosen_action]
             self.q_table[np.array(current_state),chosen_action] =curr_q_label*(1-lr) + lr* q_label
+            #self.loss.append(np.square(q_label - curr_q_label))
+            #print("loss: ",(np.square(q_label - curr_q_label)))
 
 
 
-    def test(self,summaries_collector, render=True, episodes=None):
+    def test(self,summaries_collector, render=False, episodes=None):
+        self.tests+=1
         env = self.gym_wrapper.get_env()
         episode_collector = EpisodeCollector(self.q_table, env, self.gym_wrapper.get_num_actions())
         max_episode_steps = self.config['general']['max_test_episode_steps']
@@ -100,9 +105,9 @@ class QLearning:
             avg_reward += rewards[-1]
         avg_episode_len = avg_episode_len / episodes
         avg_reward = avg_reward / episodes
-        summaries_collector.write_summaries('test', episode,avg_reward, avg_episode_len)
+        summaries_collector.write_summaries('test', self.tests,avg_reward, avg_episode_len)
 
         env.close()
-        print(self.q_table)
-        print('TEST collected rewards: {}'.format(avg_reward))
+        #print(self.q_table)
+        #print('TEST collected rewards: {}'.format(avg_reward))
         return avg_reward
